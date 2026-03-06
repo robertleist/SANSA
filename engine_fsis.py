2184bb5aa7bbb8d17cea306d683af5882f744d71
Robert Leist <robert.leist@dfki.de>
2026-03-06 08:52:36 +0100

    added sample saving

--- engine_fsis.py
+++ engine_fsis.py
@@ -1,20 +1,18 @@
 import math
-import random
 import sys
 from collections import defaultdict
-from math import ceil
-from typing import Iterable, Optional, List, Dict, Any
+from typing import Iterable, Optional
 
+import mlflow
 import numpy as np
 import torch
-from numpy.ma.extras import average
+import torch.nn.functional as F
 from torch.nn import Module
 from torch.optim import Optimizer
-import torch.nn.functional as F
-import mlflow
+
 import util.misc as utils
-from util.losses import loss_masks, loss_instances  # Assume this handles Dice/BCE for matched pairs
-from util.promptable_utils import build_prompt_dict, build_prompt_dict_fsis
+from util.losses import loss_instances  # Assume this handles Dice/BCE for matched pairs
+from util.promptable_utils import build_prompt_dict_fsis
 
 
 def train_one_epoch(
@@ -28,10 +26,11 @@ def train_one_epoch(
         args: Optional[object] = None,
 ) -> dict:
     model.train()
-    rank = utils.get_rank()
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Epoch: [{}]'.format(epoch)
     print_freq = 1
+    best_output = None
+    best_score = np.inf
 
     for i, batch in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
         global_step = epoch * len(data_loader) + i
@@ -84,6 +83,29 @@ def train_one_epoch(
             batch_loss += loss
             for k, v in metrics.items():
                 batch_metrics[k].append(v)
+            if loss < best_score:
+                best_score = loss
+                # Log artifacts
+                pred_instance_masks = (preds.sigmoid() > 0.5).bool()
+                pred_mask = pred_instance_masks[0]
+                for instance_mask in pred_instance_masks[1:]:
+                    pred_mask = torch.logical_or(pred_mask, instance_mask)
+                gt_mask = gt_instances[0]
+                for instance_mask in gt_instances[1:]:
+                    gt_mask = torch.logical_or(gt_mask, instance_mask)
+                intersection = torch.logical_and(pred_mask, gt_mask)
+                pred_only = torch.logical_and(pred_mask, torch.logical_not(intersection))
+                gt_only = torch.logical_and(gt_mask, torch.logical_not(intersection))
+                # Stack the image to RGB
+                # Red: Missed GT instances
+                # Green: Found GT instances
+                # Blue: Hallucinations
+                image = torch.stack([gt_only, intersection, pred_only], dim=-1)
+                mlflow.log_image(
+                    image=image.cpu().numpy(),
+                    key="sample",
+                    step=global_step,
+                )
 
         if not math.isfinite(batch_loss.item()):
             print(f"Loss is {batch_loss.item()}, stopping training")
@@ -108,9 +130,12 @@ def train_one_epoch(
         metric_logger.update(**avg_metrics)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
         metric_logger.update(grad_norm=grad_total_norm)
+        mlflow.log_metrics(avg_metrics, step=global_step)
+        mlflow.log_metrics({
+            "lr": optimizer.param_groups[0]["lr"],
+            "grad_norm": grad_total_norm,
+        })
 
     metric_logger.synchronize_between_processes()
-    for k, v in metric_logger.meters.items():
-        mlflow.log_metric(f"train_{k}", v.value, step=epoch)
     print("Averaged stats:", metric_logger)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
